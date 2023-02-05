c57b790c09f4ebef356d910c0bbb7afcf64248a9
IvoDBelohlav <115696259+IvoDBelohlav@users.noreply.github.com>
2023-02-05 19:42:45 +0100

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://ivodbelohlav.github.io/stopky/](https://ivodbelohlav.github.io/stopky/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/ivodbelohlav/stopky** and import
+
+## Edit this project ![Build status badge](https://github.com/ivodbelohlav/stopky/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/ivodbelohlav/stopky** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/ivodbelohlav/stopky/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,94 @@
+let start = false;
+let done = false;
+let avg = 0;
+
+function prumer(x: number) {
+    const VAHA = 3;
+    avg -= avg / VAHA;
+    avg += x;
+
+    return (avg / VAHA);
+}
+
+
+basic.forever(function () {
+    radio.setGroup(1)
+    radio.setFrequencyBand(20)
+    radio.setTransmitPower(5)
+    console.logValue("light level", input.lightLevel())
+    console.log("\n\r")
+    while (1) {
+        if (done === true)
+            continue;
+
+        if (start === false)
+            continue;
+
+        let light = prumer(input.lightLevel());
+
+        if (light < 100) {
+            radio.sendNumber(1);
+            done = true;
+        }
+    }
+})
+
+input.onButtonPressed(Button.A, function () {
+    music.playTone(Note.A, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(Note.C, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(Note.D, music.beat(BeatFraction.Whole))
+    start = true;
+})
+
+input.onButtonPressed(Button.B, function () {
+    done = false;
+})
+
+
+/////////////
+
+let start_time = 0;
+let avg = 0;
+
+function prumer(x: number) {
+    const VAHA = 3;
+
+    avg -= avg / VAHA;
+    avg += x;
+
+    return (avg / VAHA);
+}
+
+function main() {
+    while (true) {
+        let light = prumer(input.lightLevel());
+
+        if (light > 50)
+            continue;
+
+        if (start_time == 0)
+            continue;
+
+        let stop_time = control.millis();
+        console.log("cas " + (stop_time - start_time) / 1000 + " sekund");
+        start_time = 0;
+    }
+}
+
+basic.forever(function () {
+    main();
+})
+
+radio.onReceivedValue(function (name: string, value: number) {
+    if (value === 1) {
+        start_time = control.millis();
+    }
+})
+
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+
+
+})
